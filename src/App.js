6fe51004810a940ed38ab7ebd14c2072176fe48e
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-01-01 19:19:46 +0530

    Create

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
 import { db } from "./firebase-config";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 
 function App() {
-  const [users, setUsers] = useState([]);
   const usersCollectionRef = collection(db, "users");
 
+  // Read
+  const [users, setUsers] = useState([]);
+
+  // Create
+  const [newName, setNewName] = useState('');
+  const [newAge, setNewAge] = useState(0);
+
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, { name: newName, age: newAge });
+  }
+
   useEffect(() => {
+    // Read
     const getUsers = async () => {
       const data = await getDocs(usersCollectionRef);
       setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
     }
-
     getUsers();
   }, [])
 
   return (
+    // Create
     <div className="App">
+      <input type="text" value={newName} onChange={(e) => setNewName(e.target.value)} placeholder="Name" />
+      <input type="number" value={newAge} onChange={(e) => setNewAge(e.target.value)} placeholder="Age" />
+      <button onClick={createUser}>Create User</button>
+
+
+      {/* Read */}
       {users.map((user) => {
         return (
           <div>
